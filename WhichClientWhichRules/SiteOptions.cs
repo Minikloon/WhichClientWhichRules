0339a11cce3760d7502bc5d73f39232183a9213d
Minikloon <pifnpafnpouf@gmail.com>
2017-05-12 03:59:03 -0400

    Make the footer company configurable

---
+++ WhichClientWhichRules/SiteOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhichClientWhichRules
+{
+    public class SiteOptions
+    {
+		public string Company { get; set; }
+    }
+}
--- WhichClientWhichRules/Startup.cs
+++ WhichClientWhichRules/Startup.cs
@@ -46,6 +46,9 @@ namespace WhichClientWhichRules
 
 			// Add the Auth0 Settings object so it can be injected
 			services.Configure<Auth0Settings>(Configuration.GetSection("Auth0"));
+
+			// Add the SiteOptions object so it can be injected
+			services.Configure<SiteOptions>(Configuration.GetSection("Site"));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
